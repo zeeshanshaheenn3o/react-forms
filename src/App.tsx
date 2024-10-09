64b56b362eb8b0e2f37e1ff8469995fb0dc90e69
Zeeshan Shaheen <112391016+zeeshanshaheenn3o@users.noreply.github.com>
2024-10-09 15:35:10 +0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Flex, Table } from "antd";
+import type { TableColumnsType, TableProps } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type TableRowSelection<T extends object = object> =
+  TableProps<T>["rowSelection"];
+
+export interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
 }
+
+type Props = {
+  items?: DataType[];
+};
+const columns: TableColumnsType<DataType> = [
+  { title: "Name", dataIndex: "name" },
+  { title: "Age", dataIndex: "age" },
+  { title: "Address", dataIndex: "address" },
+];
+
+const App: React.FC<Props> = (props) => {
+  console.log("props : ", props);
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const start = () => {
+    setLoading(true);
+    // ajax request after empty completing
+    setTimeout(() => {
+      setSelectedRowKeys([]);
+      setLoading(false);
+    }, 1000);
+  };
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  const rowSelection: TableRowSelection<DataType> = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const hasSelected = selectedRowKeys.length > 0;
+
+  return (
+    <Flex gap="middle" vertical>
+      <Flex align="center" gap="middle">
+        <Button
+          type="primary"
+          onClick={start}
+          disabled={!hasSelected}
+          loading={loading}
+        >
+          Reload
+        </Button>
+        {hasSelected ? `Selected ${selectedRowKeys.length} items` : null}
+      </Flex>
+      <Table<DataType>
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={props.items}
+      />
+    </Flex>
+  );
+};
 
 export default App;
